90a4db3ace9377599b0e7a8c041d0ddf471bf9c9
Hermit-oss <77214495+Hermit-oss@users.noreply.github.com>
2023-03-30 02:31:24 +0200

    Title Screen
    TODO:
    - add settings button
    - add credits button
    - add play button

--- main.py
+++ main.py
@@ -1,76 +1,96 @@
 import pygame
+import os
 
 # Initialize Pygame
 pygame.init()
 
-# Get the dimensions of the screen
-screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h
-
-# Set the screen to full screen mode
-screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
-
-# Set the title of the screen
+# Set up the screen
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+width, height = screen.get_size()
 pygame.display.set_caption("Era of Conflict")
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (128, 128, 128)
+# Load assets
+game_title_font = pygame.font.Font(os.path.join("assets", "font", "Bitmgothic.ttf"), 120)
+button_font = pygame.font.Font(os.path.join("assets", "font", "Bitmgothic.ttf"), 70)
+button_hover_sound = pygame.mixer.Sound(os.path.join("assets", "snd", "hover.wav"))
 
-# Define some fonts
-title_font = pygame.font.SysFont(None, 100)
-button_font = pygame.font.SysFont(None, 50)
+# Set up game title
+game_title_text = game_title_font.render("Era of Conflict", True, (220, 220, 160))
+game_title_rect = game_title_text.get_rect(center=(width // 2, height - 60))
 
-# Create Title
-title_text = title_font.render("Era of Conflict", True, (255, 255, 255))
-title_text_rect = title_text.get_rect(center=(screen_width/2, screen_height/4))
+# Set up quit button
+button_text = button_font.render("QUIT", True, (220, 220, 160))
+button_rect = button_text.get_rect(center=(width // 7, height // 8))
+tilted_button_text = pygame.transform.rotate(button_text, 13)
 
-# Create the Play button
-play_button = pygame.Rect(screen_width//2-100, screen_height//2-50, 200, 50)
-play_text = button_font.render("Play", True, WHITE)
-play_text_rect = play_text.get_rect(center=play_button.center)
+# Load background frames
+frames = []
+for i in range(1, 53):
+    filename = os.path.join('assets/img/background', f'frame{i}.png')
+    frame = pygame.image.load(filename).convert_alpha()
+    frames.append(frame)
 
-# Create the Credits button
-credits_button = pygame.Rect(screen_width//2-100, screen_height//2+25, 200, 50)
-credits_text = button_font.render("Credits", True, WHITE)
-credits_text_rect = credits_text.get_rect(center=credits_button.center)
+# Set the initial frame index and FPS
+frame_index = 0
+FPS = 13
 
-# Create the Quit button
-quit_button = pygame.Rect(screen_width//2-100, screen_height//2+100, 200, 50)
-quit_text = button_font.render("Quit", True, WHITE)
-quit_text_rect = quit_text.get_rect(center=quit_button.center)
+# Set up hover sound flag
+hover_sound_played = False
 
-# Loop to keep the screen open
+# Load background music
+pygame.mixer.music.load("assets/snd/intro.wav")
+
+# Start playing background music
+pygame.mixer.music.play(-1)
+
+# Set up game loop
+clock = pygame.time.Clock()
 running = True
+
 while running:
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if play_button.collidepoint(mouse_pos):
-                # Start the game
-                print("Starting the game...")
-            elif credits_button.collidepoint(mouse_pos):
-                # Show the credits
-                print("Credits:")
-                print("- Created by JJEntertainment")
-            elif quit_button.collidepoint(mouse_pos):
-                # Quit the game
+            if button_rect.collidepoint(pygame.mouse.get_pos()):
                 running = False
 
-    # Draw the background and the buttons
-    screen.fill(GRAY)
-    screen.blit(title_text, title_text_rect)
-    pygame.draw.rect(screen, BLACK, play_button)
-    screen.blit(play_text, play_text_rect)
-    pygame.draw.rect(screen, BLACK, credits_button)
-    screen.blit(credits_text, credits_text_rect)
-    pygame.draw.rect(screen, BLACK, quit_button)
-    screen.blit(quit_text, quit_text_rect)
+    # Draw screen
+    screen.fill((0, 0, 0))
 
-    # Update the screen
-    pygame.display.update()
+    # Get the current frame
+    frame = frames[frame_index]
 
-# Quit Pygame
+    # Stretch and draw image
+    scaled_frame = pygame.transform.scale(frames[frame_index], screen.get_size())
+    screen.blit(scaled_frame, (0, 0))
+
+    # Update the frame index
+    frame_index = (frame_index + 1) % len(frames)
+    
+    # Draw game title
+    screen.blit(game_title_text, game_title_rect)
+
+    # Draw tilted button text
+    if button_rect.collidepoint(pygame.mouse.get_pos()):
+        tilted_button_text = pygame.transform.rotate(button_font.render("QUIT", True, (255, 0, 0)), 13)
+        if not hover_sound_played:
+            button_hover_sound.play()
+            hover_sound_played = True
+    else:
+        tilted_button_text = pygame.transform.rotate(button_font.render("QUIT", True, (220, 220, 160)), 13)
+        hover_sound_played = False
+    screen.blit(tilted_button_text, button_rect)
+
+    # Update display
+    pygame.display.flip()
+
+    # Limit the frame rate
+    clock.tick(FPS)
+
+# Stop the background music when the game is finished
+pygame.mixer.music.stop()
+
+# Quit the game
 pygame.quit()
